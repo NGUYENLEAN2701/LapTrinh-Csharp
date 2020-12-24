25088c7d435a2eabb47e936212d69b17d308f440
GreenWolf <nguyenlean2701@gmail.com>
2020-12-25 04:44:22 +0700

    ok

---
+++ Code_Example/Printing Text/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.InputEncoding = System.Text.Encoding.Unicode;
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Mr Nguyễn Lê An");
+        }
+    }
+}

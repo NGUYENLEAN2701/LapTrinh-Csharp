7c4b05fe963d6684dc0580d95971d595c72f893c
GreenWolf <nguyenlean2701@gmail.com>
2020-12-27 16:38:59 +0700

    ok man

---
+++ Code_Example/if-else/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace if_else
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 33;
+
+            if (x == 8)
+            {
+                Console.WriteLine("Value of x is 8");
+            }
+            else if (x == 18)
+            {
+                Console.WriteLine("Value of x is 18");
+            }
+            else if (x == 33)
+            {
+                Console.WriteLine("Value of x is 33");
+            }
+            else
+            {
+                Console.WriteLine("No match");
+            }
+
+        }
+    }
+}

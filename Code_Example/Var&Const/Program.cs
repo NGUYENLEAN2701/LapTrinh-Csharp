08143f1b92ba1944874193b85dbc923a66208911
GreenWolf <nguyenlean2701@gmail.com>
2020-12-27 15:03:36 +0700

    ok

---
+++ Code_Example/Var&Const/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Var_Const
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var x = 2.2;
+            const double pi = 3.14;
+            Console.WriteLine(pi);
+            Console.WriteLine(x);
+
+        }
+    }
+}

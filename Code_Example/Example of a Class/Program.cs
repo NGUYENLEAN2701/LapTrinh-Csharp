48349211c769a7a7d21507844d71dcbde9e430bd
GreenWolf <nguyenlean2701@gmail.com>
2020-12-28 14:49:24 +0700

    ok

---
+++ Code_Example/Example of a Class/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_of_a_Class
+{
+    class Program
+    {
+        class Person
+        {
+            int age = 1;
+            string name = "name";
+            public void SayHi()
+            {
+                Console.WriteLine("Hi");
+                Console.WriteLine(age);
+                Console.WriteLine(name);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Person p1 = new Person();
+            p1.SayHi();
+        }
+    }
+
+}

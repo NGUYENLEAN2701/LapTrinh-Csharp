7c7d84117e718a57054862a8961ddd6ae3e7b780
GreenWolf <nguyenlean2701@gmail.com>
2020-12-29 04:19:22 +0700

    ok

---
+++ Code_Example/auto-property/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoloLearn
+{
+    class Program
+    {
+        class Person
+        {
+            public string Name { get; set; }
+        }
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+            p.Name = "Bob";
+            Console.WriteLine(p.Name);
+        }
+    }
+}

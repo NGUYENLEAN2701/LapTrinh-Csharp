22a4c835fa4c37c4ff24d37feba5a62c52d75983
Lê An Nguyễn <nguyenlean2701@hotmail.com>
2021-01-10 22:07:31 +0700

    ok

---
+++ Project SoloLearn/3-Level Points/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int levels = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(Points(levels));
+        }
+
+        static int Points(int levels)
+        {
+            //your code goes here
+            return (levels * (levels + 1)) / 2;
+        }
+    }
+}

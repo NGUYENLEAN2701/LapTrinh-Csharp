80b1eefe7dec2999d7261166005c34dfbf7e6dbf
Lê An Nguyễn <nguyenlean2701@hotmail.com>
2021-01-10 23:30:58 +0700

    okyyy

--- Code_Example/Unicode/Program.cs
+++ Code_Example/Unicode/Program.cs
@@ -11,6 +11,7 @@ namespace SoloLearn
     {
         static void Main(string[] args)
         {
+
             Console.OutputEncoding = System.Text.Encoding.Unicode;
             Console.InputEncoding = System.Text.Encoding.Unicode;
             Console.WriteLine("Hello World!");

a7dfecb9ce9c149595e467109d3c65119e20d7b0
Lê An Nguyễn <nguyenlean2701@hotmail.com>
2021-02-02 20:45:16 +0700

    ok

--- Codelearn.io/Cấu trúc dữ liệu và giải thuật/7-Cấu Trúc Dữ Liệu Cây/62-thành lập cây/Program.cs
+++ Codelearn.io/Cấu trúc dữ liệu và giải thuật/7-Cấu Trúc Dữ Liệu Cây/62-thành lập cây/Program.cs
@@ -2,11 +2,53 @@
 
 namespace _62_thành_lập_cây
 {
+    class Node
+    {
+        public int key;
+        public Node left, right;
+        public Node(int d)
+        {
+            key = d;
+        }
+    }
+    class BTree
+    {
+        public Node root;
+        public Node insert(Node node, int key)
+        {
+            if (node == null)
+                return (new Node(key));
+            else
+            {
+                if (key < node.key)
+                    node.left = insert(node.left, key);
+                else
+                    node.right = insert(node.right, key);
+            }
+            return node;
+        }
+        public void printTree(Node node)
+        {
+            if (node != null)
+            {
+                printTree(node.left);
+                Console.Write(node.key + " ");
+                printTree(node.right);
+            }
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BTree tree = new BTree();
+
+
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+                tree.root = tree.insert(tree.root, Convert.ToInt32(Console.ReadLine()));
+
+            tree.printTree(tree.root);
         }
     }
 }
